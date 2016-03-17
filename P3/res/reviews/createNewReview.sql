ed74ca0ea85a3f10d05eb7c466514c7d0ef19c54
Oleg Zhilin <o.zhilin1@gmail.com>
2016-03-16 20:10:44 -0400

    Added get and create reviews.

---
+++ P3/res/reviews/createNewReview.sql
@@ -0,0 +1,2 @@
+INSERT INTO Reviews (email, bid, review_date, review, rating) 
+VALUES (?, ?, ?, ?, ?);
---
+++ P3/src/db/managers/ReviewManager.java
@@ -0,0 +1,67 @@
+package db.managers;
+
+import java.io.FileNotFoundException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.models.Review;
+import db.models.User;
+import db.util.QueryHelper;
+
+public class ReviewManager extends AModelManager {
+	public void createNewReview(Review review, int bookingId, User user) {
+		// Only a host can create
+		if (!user.isLoggedIn()) {
+			return;
+		}
+		
+		try {
+			String query = QueryHelper.findQuery("reviews/createReview.sql");
+			PreparedStatement stmnt = conn.prepareStatement(query);
+			
+			stmnt.setString(1, user.email);
+			stmnt.setInt(2, bookingId);
+			stmnt.setDate(3, new java.sql.Date(review.reviewDate.getTime()));
+			stmnt.setString(4, review.review);
+			stmnt.setInt(5, review.rating);
+			
+			stmnt.executeUpdate();
+			conn.commit();
+			
+			ResultSet rs = stmnt.getGeneratedKeys();
+			rs.next();
+		} catch (SQLException e) {
+			System.out.println("Create review failed");
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not find createReview query");
+		}
+	}
+	public List<Review> getReviewByLid(int lid) {
+		List<Review> result = new ArrayList<Review>();
+		
+		try {
+			String query = QueryHelper.findQuery("lodgings/getReviewsByLid.sql");
+			
+			PreparedStatement stmnt = conn.prepareStatement(query);
+			stmnt.setInt(1, lid);
+			
+			ResultSet rs = stmnt.executeQuery(query);
+			
+			while (rs.next()) {
+				result.add(new Review(rs));
+			}
+			
+			return result;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+			
+		return null;
+	}
+}
