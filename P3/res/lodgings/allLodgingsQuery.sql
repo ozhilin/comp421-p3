4a9dbba3a8d4e3f8fe4938cae40b4471936e8473
Oleg Zhilin <o.zhilin1@gmail.com>
2016-03-20 14:30:34 -0400

    fixed all lodgings query

--- P3/res/lodgings/allLodgingsQuery.sql
+++ P3/res/lodgings/allLodgingsQuery.sql
@@ -1,6 +1,10 @@
 SELECT * 
-FROM Lodgings L, Address A, 
-  (SELECT lid, AVG(rating) as avg_rating
-    FROM Bookings B LEFT OUTER JOIN Reviews R ON B.bid = R.bid
-    GROUP BY lid) AS lid_rating
-WHERE L.aid = A.aid AND lid_rating.lid = L.lid;
+FROM Lodgings L
+  JOIN 
+    Address A 
+    ON L.aid = A.aid
+  LEFT OUTER JOIN
+    (SELECT lid, AVG(rating) as avg_rating
+      FROM Bookings B LEFT OUTER JOIN Reviews R ON B.bid = R.bid
+      GROUP BY lid) AS lid_rating
+    ON lid_rating.lid = L.lid;
